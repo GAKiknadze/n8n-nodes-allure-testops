1c5ac89232d849c21909bdb058b959008a2aad8e
GAKiknadze <gakiknadze@yandex.ru>
2025-07-23 20:06:42 +0300

    Added credential type AllureTestOpsCredentialsApi

---
+++ credentials/AllureTestOpsCredentialsApi.credentials.ts
@@ -0,0 +1,45 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class AllureTestOpsCredentialsApi implements ICredentialType {
+	name = 'allureTestOpsCredentialsApi';
+	displayName = 'Allure TestOps Credentials API';
+	documentationUrl = 'https://github.com/GAKiknadze/n8n-nodes-allure-testops/docs/credentials.md';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			required: true,
+			default: '',
+		},
+		{
+			displayName: 'Domain',
+			name: 'domain',
+			type: 'string',
+			required: true,
+			default: '',
+			placeholder: 'https://sandbox.testops.cloud',
+		},
+	];
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '=Bearer {{$credentials.authToken}}',},
+		}
+	};
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials.domain}}',
+			url: '/api/project',
+		},
+	};
+}
